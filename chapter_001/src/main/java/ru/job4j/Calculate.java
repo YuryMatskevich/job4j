636c47c0efdc6a69488f292c92bdb6362d3899bc
Yury Matskevich <y.n.matskevich@gmail.com>
2017-12-18 17:22:17 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j;
 
+/**
+*class Calculate решение задачи части 001 урок1.
+*@author Yury Matskevich
+*@since 18.12.2017
+*/
+
 public class Calculate {
+/**
+*Метод main, вывод строки в консоль.
+*@param args - arg.
+*/
 	public static void main(String[] args) {
 		System.out.print("Hello world");
 	}
