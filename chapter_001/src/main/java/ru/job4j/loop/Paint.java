2d715ea1a451fc75ab12744e8417d8767a61cd73
Yury Matskevich <y.n.matskevich@gmail.com>
2017-12-24 21:24:56 +0300

    4.4. Построить пирамиду в псевдографике. [#4412]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -0,0 +1,24 @@
+package ru.job4j.loop;
+
+/**
+ * @author Yury Matskevich(y.n.matskevich@gmail.com)
+ * @version $Id$
+ * @since 24.12.2017
+ */
+public class Paint {
+    public String pyramid(int height) {
+        StringBuilder screen = new StringBuilder();
+        int weight = 2 * height - 1;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != weight; column++) {
+                if (row >= height - column - 1 && row + height - 1 >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+}
