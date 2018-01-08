6d8c36c086f33b1096f2c8faa8cae8e4d4583717
Yury Matskevich <y.n.matskevich@gmail.com>
2018-01-08 12:10:34 +0300

    Дополнительное задание(Исправление 1)

--- chapter_001/src/main/java/ru/job4j/array/SortArray.java
+++ chapter_001/src/main/java/ru/job4j/array/SortArray.java
@@ -5,35 +5,35 @@ package ru.job4j.array;
  * @since 0.1
  */
 public class SortArray {
-    public int[] sort(int[] firstArray, int[] secondArray) {
+    public int[] sort(int[] array1, int[] array2) {
 
-        int fSize = firstArray.length;
-        int sSize = secondArray.length;
-        int[] array = new int[fSize + sSize];
+        int size1 = array1.length;
+        int size2 = array2.length;
+        int[] array = new int[size1 + size2];
 
-        int fI = 0;
-        int sI = 0;
+        int count1 = 0;
+        int count2 = 0;
 
-        int[] current = null;
-        int iCurrent = 0;
+        int[] cur = null;
+        int total = 0;
 
-        for (int i = 0; i < fSize + sSize; i++) {
-            if (fI == fSize | sI == sSize) {
-                for (int y = iCurrent; y < current.length; y++) {
-                    array[i] = current[y];
+        for (int i = 0; i < size1 + size2; i++) {
+            if (count1 == size1 | count2 == size2) {
+                for (int y = total; y < cur.length; y++) {
+                    array[i] = cur[y];
                     i++;
                 }
             } else {
-                if (firstArray[fI] < secondArray[sI]) {
-                    array[i] = firstArray[fI];
-                    fI++;
-                    current = secondArray;
-                    iCurrent = sI;
+                if (array1[count1] < array2[count2]) {
+                    array[i] = array1[count1];
+                    count1++;
+                    cur = array2;
+                    total = count2;
                 } else {
-                    array[i] = secondArray[sI];
-                    sI++;
-                    current = firstArray;
-                    iCurrent = fI;
+                    array[i] = array2[count2];
+                    count2++;
+                    cur = array1;
+                    total = count1;
                 }
             }
         }
