3aea4493cff307a97ff68bdee7fa8347f40cdb64
Yury Matskevich <y.n.matskevich@gmail.com>
2018-01-24 11:51:13 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -2,8 +2,15 @@ package ru.job4j.tracker;
 
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 
 
@@ -12,6 +19,23 @@ import static org.junit.Assert.assertThat;
  * @since 0.1
  */
 public class StartUITest {
+
+    private final PrintStream stdout = System.out;
+
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     /**
      * Добовление заявки
      */
@@ -29,9 +53,9 @@ public class StartUITest {
     public void whenUpdateThenTrackerHasUpdatedValue() {
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("test name", "desc", 123L));
-        Input input = new StubInput(new String[]{"1", item.getId(), "test name", "desc", "6"});
+        Input input = new StubInput(new String[]{"2", item.getId(), "test name1", "desc", "6"});
         new StartUI(input, tracker).init();
-        assertThat(tracker.findById(item.getId()).getName(), is("test name"));
+        assertThat(tracker.findById(item.getId()).getName(), is("test name1"));
     }
     /**
      * Удаление заявки
@@ -45,5 +69,53 @@ public class StartUITest {
         Item item = tracker.findAll()[0];
         tracker.delete(item.getId());
         assertThat(tracker.findAll()[0].getName(), is("test name2"));
+    }
+    /**
+     * Просмотр заявок в трекере
+     */
+    @Test
+    public void whenShowAllTheItems() {
+        Tracker tracker = new Tracker();
+        Input input = new StubInput(new String[]{"0", "test name1", "desc1", "0", "test name2", "desc2", "1", "6"});
+        new StartUI(input, tracker).init();
+        tracker.findAll();
+
+        String string = new String(out.toByteArray());
+
+        String subString1 = "Имя заявки: test name1\r\nОписание заявки: desc1\r\n";
+        String subString2 = "Имя заявки: test name2\r\nОписание заявки: desc2\r\n";
+
+        assertFalse(string.lastIndexOf(subString1) < 0);
+        assertFalse(string.lastIndexOf(subString2) < 0);
+    }
+    /**
+     * Поиск завявки по Id
+     */
+    @Test
+    public void whenFindItemById() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc", 123L));
+        Input input = new StubInput(new String[]{"4", item.getId(), "6"});
+        new StartUI(input, tracker).init();
+
+        String string = new String(out.toByteArray());
+        String subString = "Id заявки: " + item.getId() + "\r\n" + "Имя заявки: test name\r\nОписание заявки: desc\r\n";
+
+        assertFalse(string.lastIndexOf(subString) < 0);
+    }
+    /**
+     * Поиск завявки по имени
+     */
+    @Test
+    public void whenFindItemByName() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc", 123L));
+        Input input = new StubInput(new String[]{"5", item.getName(), "6"});
+        new StartUI(input, tracker).init();
+
+        String string = new String(out.toByteArray());
+        String subString = "Id заявки: " + item.getId() + "\r\n" + "Имя заявки: " + item.getName() + "\r\n" + "Описание заявки: desc\r\n";
+
+        assertFalse(string.lastIndexOf(subString) < 0);
     }
 }
