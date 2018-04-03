e4d0a9184c21332aa27e702d2862bd6129e08d52
Yury Matskevich <y.n.matskevich@gmail.com>
2018-04-03 15:09:14 +0300

    2. Set реализованный на связном списке (Исправление 1)

--- chapter_004/src/test/java/ru/job4j/set/SimpleSetNodeTest.java
+++ chapter_004/src/test/java/ru/job4j/set/SimpleSetNodeTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.set;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Iterator;
@@ -13,20 +14,33 @@ import static org.junit.Assert.*;
  * @since 0.1
  */
 public class SimpleSetNodeTest {
+    SimpleSetNode<String> set;
+    Iterator<String> it;
 
-    @Test(expected = NoSuchElementException.class)
-    public void showSet() {
-        SimpleSetNode<String> set = new SimpleSetNode<>();
+    @Before
+    public void setUp() {
+        set = new SimpleSetNode<>();
         set.add("first");
         set.add("first");
         set.add("first");
         set.add("second");
         set.add("second");
         set.add("third");
-        Iterator<String> it = set.iterator();
+        it = set.iterator();
+    }
+
+    @Test
+    public void whenAddElementThenIsAddedOnlyUniqueElement() {
         assertThat(it.next(), is("first"));
         assertThat(it.next(), is("second"));
         assertThat(it.next(), is("third"));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenIteratorDoesNotHaveAnyMoreElementThenNoSuchElementException() {
+        it.next();
+        it.next();
+        it.next();
         it.next();
     }
 }
