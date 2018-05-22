f063a9dbcd4a01f299c6c4a74e933fd139a51f13
Yury Matskevich <y.n.matskevich@gmail.com>
2018-05-22 16:27:30 +0300

    3. Реализовать собственный механизм блокировок Lock (Исправление 1)

--- chapter_005/src/main/java/ru/job4j/jmm/Text.java
+++ chapter_005/src/main/java/ru/job4j/jmm/Text.java
@@ -1,17 +1,11 @@
 package ru.job4j.jmm;
 
-import ru.job4j.wait.ILock;
-import ru.job4j.wait.RealizationLock;
-
 /**
  * @author Yury Matskevich
  * @since 0.1
  */
 public class Text {
-	private ILock lock = new RealizationLock();
-
     public void print(String text) {
-    	lock.lock();
         System.out.format("[%s", text);
         try {
             Thread.sleep(500);
@@ -19,6 +13,5 @@ public class Text {
             System.out.println("Прерван");
         }
         System.out.println("]");
-        lock.unlock();
     }
 }
--- chapter_005/src/test/java/ru/job4j/wait/RealizationLockTest.java
+++ chapter_005/src/test/java/ru/job4j/wait/RealizationLockTest.java
@@ -3,8 +3,6 @@ package ru.job4j.wait;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import ru.job4j.jmm.MyThread;
-import ru.job4j.jmm.Text;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -30,29 +28,75 @@ public class RealizationLockTest {
 		System.setOut(stout);
 	}
 
-	/*
-	Возьмем пример из задания про демонстрацию проблем с многопотчностью
-	Но теперь метод класса Text имеет замок который закрывается когда поток
-	начинает выполнять его (при этом другие потоки не могут его выполнять,
-	они находятся в режиме "ожидани"), и открвается когда он его закончит.
-	 */
-	@Test
-	public void testPrint() throws InterruptedException {
-		Text text = new Text();
-		new MyThread(text, "first");
-		new MyThread(text, "second");
-		new MyThread(text, "third");
-		Thread.sleep(3000);
+	private class Factorial {
+		private int result;
+		private ILock lock;
 
-		Set<String> setExpected = new TreeSet<>(
-				Arrays.asList("[first]", "[second]", "[third]")
-		);
-		Set<String> set = new TreeSet<>(
+		public Factorial(ILock lock) {
+			this.lock = lock;
+		}
+
+		private int fact(int n) {
+			if (n == 1) {
+				return 1;
+			}
+			result = fact(n - 1) * n;
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			return result;
+		}
+
+		public void getFactorial(int value) {
+			lock.lock();
+			System.out.println(fact(value));
+			lock.unlock();
+		}
+	}
+
+	@Test
+	public void factorialTest() throws InterruptedException {
+		ILock lock = new RealizationLock();
+		Factorial factorial = new Factorial(lock);
+
+		Thread thread1 = new Thread() {
+			@Override
+			public void run() {
+				lock.unlock();
+				factorial.getFactorial(10);
+			}
+		};
+		Thread thread2 = new Thread() {
+			@Override
+			public void run() {
+				lock.unlock();
+				factorial.getFactorial(5);
+			}
+		};
+		Thread thread3 = new Thread() {
+			@Override
+			public void run() {
+				lock.unlock();
+				factorial.getFactorial(6);
+			}
+		};
+		thread1.start();
+		thread2.start();
+		thread3.start();
+		thread1.join();
+		thread2.join();
+		thread3.join();
+		Set<String> resultSet = new HashSet<>(
 				Arrays.asList(
 						new String(out.toByteArray())
 								.split(System.lineSeparator())
 				)
 		);
-		assertEquals(setExpected, set);
+		Set<String> expectedSet = new HashSet<>(
+				Arrays.asList("3628800", "120", "720")
+		);
+		assertEquals(expectedSet, resultSet);
 	}
 }
