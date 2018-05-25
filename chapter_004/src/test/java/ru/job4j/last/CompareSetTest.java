46312b5e3cd3d9239b7d4d643dcf59d06c39abb3
Yury Matskevich <y.n.matskevich@gmail.com>
2018-05-25 16:30:08 +0300

    Дополнительная задача №2 (Сравнить 2-а множества)(Доработка)

--- chapter_004/src/test/java/ru/job4j/last/CompareSetTest.java
+++ chapter_004/src/test/java/ru/job4j/last/CompareSetTest.java
@@ -1,6 +1,9 @@
 package ru.job4j.last;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -9,20 +12,38 @@ import static org.junit.Assert.*;
  * @since 0.1
  */
 public class CompareSetTest {
+	private CompareSet<String> compareSet;
+
+	@Before
+	public void setUp() {
+		compareSet = new CompareSet<>();
+	}
 
 	@Test
 	public void whenTwoSetsAreEqualEachOther() {
 		String[] array1 = {"A", "B", "C", "D"};
 		String[] array2 = {"B", "C", "A", "D"};
-		CompareSet<String> compareSet = new CompareSet<>(array1, array2);
-		assertTrue(compareSet.equalSet());
+		assertTrue(compareSet.equalSet(array1, array2));
 	}
 
 	@Test
 	public void whenTwoSetsAreNotEqualEachOther() {
 		String[] array1 = {"A", "B", "C", "D", "E"};
 		String[] array2 = {"B", "C", "A", "D"};
-		CompareSet<String> compareSet = new CompareSet<>(array1, array2);
-		assertFalse(compareSet.equalSet());
+		assertFalse(compareSet.equalSet(array1, array2));
+	}
+
+	@Test
+	public void whenTwoDozensAreEqualEachOther() {
+		String[] array1 = {"A", "A", "B"};
+		String[] array2 = {"B", "A", "A"};
+		assertTrue(compareSet.equalSet(array1, array2));
+	}
+
+	@Test
+	public void whenTwoDozensAreNotEqualEachOther() {
+		String[] array1 = {"A", "A", "B"};
+		String[] array2 = {"B", "B", "A"};
+		assertFalse(compareSet.equalSet(array1, array2));
 	}
 }
