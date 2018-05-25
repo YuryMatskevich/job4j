3d97d15f186164550caf66c14830978c48bb4ad4
Yury Matskevich <y.n.matskevich@gmail.com>
2018-05-25 14:58:57 +0300

    Дополнительная задача №1 (ПИТЕР-СЕРВИС)(доработка тестов)

--- chapter_004/src/test/java/ru/job4j/last/TrieMapTest.java
+++ chapter_004/src/test/java/ru/job4j/last/TrieMapTest.java
@@ -3,7 +3,7 @@ package ru.job4j.last;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.*;
 
 import static org.junit.Assert.*;
@@ -25,10 +25,41 @@ public class TrieMapTest {
 		index.loadFile(path);
 	}
 
+	//Текст в файлу в строку
+	private String convertFileToString() throws IOException {
+		DataInputStream dis = new DataInputStream(new FileInputStream(path));
+		byte[] bytes = new byte[dis.available()];
+		dis.readFully(bytes);
+		return new String(bytes, 0, bytes.length);
+	}
+
+	//преобразуем стрку в массив, каждый новый элемент которого содержит след слово.
+	private String[] createArrayOfStringWhichIncludsOnliWords() throws IOException {
+		String[] cur = convertFileToString().split("\\W");
+		List<String> temp = new ArrayList<>();
+		for (String item : cur) {
+			temp.add(item.toLowerCase());
+		}
+		return temp.toArray(new String[temp.size()]);
+	}
+
+	//возвращает Set позиций слова(позиция слова тексте: 1-первое слово, 2-второе слово и т.д.)
+	private Set<Integer> getExpectedPosition(String word) throws IOException {
+		Set<Integer> result = new TreeSet<>();
+		int i = 0;
+		for (String item : createArrayOfStringWhichIncludsOnliWords()) {
+			i++;
+			if (item.equals(word)) {
+				result.add(i);
+			}
+		}
+		return result;
+	}
+
 	@Test
-	public void whenSearchForWordWhichConsistsOfOneLatterAndExistsInLoadedText() {
-		Set<Integer> setExpected = new TreeSet<>(Arrays.asList(3, 15, 20, 28, 33, 40));
-		assertEquals(index.getIndexes4Word("a"), setExpected);
+	public void whenSearchForWordWhichConsistsOfOneLatterAndExistsInLoadedText() throws IOException {
+		String search = "a";
+		assertEquals(index.getIndexes4Word(search), getExpectedPosition(search));
 	}
 
 	@Test
@@ -42,9 +73,9 @@ public class TrieMapTest {
 	}
 
 	@Test
-	public void whenSearchForWordWhichExistsInLoadedTextThenReturnsSetOfPositionThisWordInTheText() {
-		Set<Integer> setExpected = new TreeSet<>(Arrays.asList(9, 22));
-		assertEquals(index.getIndexes4Word("called"), setExpected);
+	public void whenSearchForWordWhichExistsInLoadedTextThenReturnsSetOfPositionThisWordInTheText() throws IOException {
+		String search = "called";
+		assertEquals(index.getIndexes4Word(search), getExpectedPosition(search));
 	}
 
 	@Test
